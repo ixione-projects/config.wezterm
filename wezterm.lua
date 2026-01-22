29503e351cf7c69fcc6c852b42c4ae8d65f321d2
Treyvon Whitaker <ixione.dev@gmail.com>
2026-01-21 19:40:52 -0600

    chore(config): update inital dims

--- wezterm.lua
+++ wezterm.lua
@@ -2,19 +2,13 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.initial_cols = 80
-config.initial_rows = 24
+config.initial_cols = 120
+config.initial_rows = 28
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 12
 
-local now = os.date("*t", os.time())
-local frac_hour = now.hour + (now.min / 60) + (now.sec / 3600)
-if frac_hour >= 7 and frac_hour < 19 then
-	config.color_scheme = "tokyonight_day"
-else
-	config.color_scheme = "tokyonight_night"
-end
+config.color_scheme = "tokyonight_night"
 
 config.window_background_opacity = 0.8
 
